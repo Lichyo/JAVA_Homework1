6bbd2bcff2734ef7489fea748b2854432f8ef7ab
Lichyo <chyoxuan@gmail.com>
2022-10-05 14:27:53 +0800

    Initial

---
+++ HomeWork1.md
@@ -0,0 +1,21 @@
+# JAVA質數作業
+
+## Author
+- Name: Lichyo
+- ID: 111016041
+- class: 資一甲
+- email: chyoxuan@gmail.com
+
+## Input
+- 輸入一個大於0的整數
+
+## Output
+- 判斷輸入數字使否為質數
+    - 是則輸出“是”
+    - 不是則輸出“否”
+
+## 加分項目
+- 顯示因式分解判斷是否正確
+
+
+
---
+++ HomeWork_1.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class HomeWork_1
+{
+    public static void main(String args[])
+    {
+        /*
+         * 質數作業 
+         * Name: Lichyo
+         * Class: 資二甲
+         * ID: 111016041
+         */
+         // Declares   
+         int input = 0; // User's Input
+         int counter = 0;
+         Scanner scn = new Scanner(System.in);
+
+         
+         System.out.println("Please input a number x, x>0. ");
+         input = scn.nextInt();
+
+         // Input a Integer x that x > 0.
+         while(input <= 0)
+         {
+            System.out.println("x <1, Please input x again. ");
+            input = scn.nextInt();
+         }
+
+         // Run the Result
+         for( int detector = 2; detector < input; detector++)
+         {
+            if( (input % detector) == 0 )
+            {
+                counter++;
+                break;
+            }
+         }
+
+         if(counter == 1 )
+            System.out.println("是");
+         else if(counter ==0)
+            System.out.println("否");
+         
+        scn.close();
+    }
+}
