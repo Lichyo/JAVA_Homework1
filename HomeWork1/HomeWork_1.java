0e34482635cfb3d1febc047b47f8c82ce470b720
Lichyo <chyoxuan@gmail.com>
2022-10-06 16:05:40 +0800

    1. functions takeout from main. 2. revise the error of prime number 2.

--- HomeWork1/HomeWork_1.java
+++ HomeWork1/HomeWork_1.java
@@ -1,32 +1,35 @@
 import java.util.Scanner;
+/*
+* 質數作業 
+* Name: Lichyo
+* Class: 資二甲
+* ID: 111016041
+*/
 
 public class HomeWork_1
 {
-    public static void main(String args[])
+    public static int Input()   // Input a Integer x that x > 0.
     {
-        /*
-         * 質數作業 
-         * Name: Lichyo
-         * Class: 資二甲
-         * ID: 111016041
-         */
-         // Declares   
-         int input = 0; // User's Input
-         int counter = 0;
-         Scanner scn = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);
+        int input = 0;
+        
+        System.out.println("Please input a number x, x>0. ");
+        //System.out.println("If you input 0, then the program will close. ");
+        input = scn.nextInt();
 
-         System.out.println("Please input a number x, x>0. ");
-         input = scn.nextInt();
-
-         // Input a Integer x that x > 0.
-         while(input <= 0)
+        while(input <= 0)
          {
             System.out.println("x <1, Please input x again. ");
             input = scn.nextInt();
          }
+         scn.close(); 
+         return input;
+    }
 
-         // Run the Result
-         for( int detector = 2; detector < input; detector++)
+    public static void Result(int input)
+    {
+        int counter = 0;
+        for( int detector = 2; detector < input; detector++)
          {
             if( (input % detector) == 0 )
             {
@@ -34,12 +37,16 @@ public class HomeWork_1
                 break;
             }
          }
-
-         if(counter == 1 )
-            System.out.println("是");
+         if(counter == 1 || input == 2)
+         System.out.println("是");
          else if(counter ==0)
-            System.out.println("否");
-         
-        scn.close();
+         System.out.println("否");
+    }
+
+    public static void main(String args[])
+    {   
+        int input = 0;      //Declare a Variable that User should Input.
+        input = Input();    //User Input.
+        Result(input);      // Run the prime number's Result.
     }
 }
