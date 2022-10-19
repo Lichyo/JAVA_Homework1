13aad8585aae965df3bb46fba96b5aaa0081b440
Lichyo <chyoxuan@gmail.com>
2022-10-19 15:39:06 +0800

    Group functions into PrimeFac Class. Use Constructor to call the function.

--- HomeWork1/HomeWork_1.java
+++ HomeWork1/HomeWork_1.java
@@ -6,27 +6,31 @@ import java.util.Scanner;
 * ID: 111016041
 */
 
-public class HomeWork_1
+class PrimeFac
 {
-    public static int Input()   // Input a Integer x that x > 0.
+    int input;
+    int detector;
+    int counter;
+
+    int input()   // Input a Integer x that x > 0.
     {
         Scanner scn = new Scanner(System.in);
-        int input = 0;
+        input = 0;
         
         System.out.println("Please input a number x, x>0. ");
         //System.out.println("If you input 0, then the program will close. ");
         input = scn.nextInt();
 
         while(input <= 0)
-         {
+        {
             System.out.println("x <1, Please input x again. ");
             input = scn.nextInt();
-         }
-         scn.close(); 
-         return input;
+        }
+        scn.close(); 
+        return input;
     }
 
-    public static void Result(int input)
+    void result(int input)
     {
         int counter = 0;
         for( int detector = 2; detector < input; detector++)
@@ -42,11 +46,13 @@ public class HomeWork_1
          else if(counter ==0)
          System.out.println("否");
     }
+}
 
+public class HomeWork_1
+{
     public static void main(String args[])
     {   
-        int input = 0;      //Declare a Variable that User should Input.
-        input = Input();    //User Input.
-        Result(input);      // Run the prime number's Result.
+        PrimeFac prime = new PrimeFac();
+        prime.result(prime.input());
     }
 }
