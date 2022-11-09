d26a389f33c2cde563d4ea2eff7bbb40caf05221
Lichyo <chyoxuan@gmail.com>
2022-11-09 22:34:45 +0800

    WRONGgit add .

--- HomeWork2/H2_111016041.java
+++ HomeWork2/H2_111016041.java
@@ -4,42 +4,51 @@ public class H2_111016041
 {
     public static void main(String [] args)
     {   
-        int arraySpace = 0;
+        int max = 0, min = 0;
         Scanner scn = new Scanner(System.in);
-        
-        System.out.println("Please input a number x, x>1. ");
-        // while(!scn.hasNextInt() ||  scn.nextInt() <1)
-        // {
-        //     System.out.println("輸入錯誤！！！");
-        //     System.out.printf("請重新輸入：");
-        // }
-        arraySpace = scn.nextInt();
-
-        int array [] = new int[arraySpace];
         System.out.println("請依序輸入欲排序數字，並以空白間隔每個數字");
         System.out.println("例如：2 3 4 7 11 6");
-        for(int count = 0; count < arraySpace; count++)
-        {
-            array[count] = scn.nextInt();
-        }
+        String[] input= scn.nextLine().split(" ");
         scn.close();
-
-        int temp = 0;
-        for(int i = 0; i < arraySpace; i++)
+        int [] array = new int[input.length];
+        for(int i=0;i<input.length;i++) // 偵測有沒有輸入非數字相關符號字元，如果有將跳出程式，沒有就將String 轉成 int
         {
-            for(int j = 0; j < arraySpace-1; j++)
+            try
             {
-                if(array[j] < array[j+1])
-                {
-                    temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp; 
-                }
+                array[i] = Integer.parseInt(String.valueOf(input[i]));        
+            }catch(Exception e){
+                System.out.println("輸入錯誤，將退出程式");
+                return;     // 離開程式
             }
         }
-        for(int i = 0; i< arraySpace; i++)
+        max = 0;
+        min = 0;
+        for(int i = 0; i < array.length; i++)   // 找最大最小
         {
-            System.out.printf(array[i] + " ");
+            if(array[i] > max)
+            {
+                max = array[i];
+                // System.out.println(array[i]);
+            }
+            if(array[i] < min)
+            {
+                min = array[i];
+                System.out.println(array[i]);
+            }
+        }
+        int temp = 0;
+        // System.out.println(min);
+        for(int j = 0; j < array.length; j++)
+        {
+            for(int i = 0; i < array.length; i++)
+            {
+                if(array[i] == min)
+                {
+                    min = array[i];
+                    array[i] = max;
+                }
+            }
+            System.out.printf(array[j] + " ");
         }
     }    
 }
