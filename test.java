1a34abe5a9d6d901542c3f1b1864e57836689f58
Lichyo <chyoxuan@gmail.com>
2022-11-09 22:35:01 +0800

    WRONG

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String [] args)
+    {
+        String number = "-2224";
+        int number_1 = Integer.parseInt(number);
+        System.out.println(number_1);
+    }
+}
