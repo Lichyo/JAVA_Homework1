dac842c801324c82dd361cbe4f56c13ad65ec511
Lichyo <chyoxuan@gmail.com>
2022-11-16 21:32:12 +0800

    Start Homework3.

--- HomeWork2/H2_111016041.java
+++ HomeWork2/H2_111016041.java
@@ -4,6 +4,15 @@ public class H2_111016041
 {
     public static void main(String [] args)
     {   
+        /*
+        * Name: 李其祐
+        * Class: 資二甲
+        * ID: 111016041
+        * 自評分數：90
+        * 有做到輸出正確結果、擋非數字，但是程式寫的不太好看
+        */
+
+
         int max = 0, min = 0;
         Scanner scn = new Scanner(System.in);
         System.out.println("請依序輸入欲排序數字，並以空白間隔每個數字");
@@ -23,32 +32,44 @@ public class H2_111016041
         }
         max = 0;
         min = 0;
-        for(int i = 0; i < array.length; i++)   // 找最大最小
-        {
-            if(array[i] > max)
-            {
-                max = array[i];
-                // System.out.println(array[i]);
-            }
-            if(array[i] < min)
-            {
-                min = array[i];
-                System.out.println(array[i]);
-            }
-        }
-        int temp = 0;
-        // System.out.println(min);
-        for(int j = 0; j < array.length; j++)
-        {
-            for(int i = 0; i < array.length; i++)
-            {
-                if(array[i] == min)
-                {
-                    min = array[i];
-                    array[i] = max;
+        // for(int i = 0; i < array.length; i++)   // 找最大最小
+        // {
+        //     if(array[i] > max)
+        //     {
+        //         max = array[i];
+        //         // System.out.println(array[i]);
+        //     }
+        //     if(min > array[i])
+        //     {
+        //         min = array[i];
+        //         //System.out.println(array[i]);
+        //     }
+        // }
+        // System.out.println("max = " + max);
+        // System.out.println("min = " + min);
+        int [] temp = new int[array.length];
+
+        int pointer=0;
+        // 找最大最小，然後把數字排進temp
+        for(int j=0;j<array.length;j++)
+        {			
+            max = min;
+            pointer = 0;
+            for(int i = 0;i < array.length; i++)
+            {		
+                if(max < array[i])
+                {					
+                    max = array[i];
+                    pointer = i;
                 }
-            }
-            System.out.printf(array[j] + " ");
+            }	
+            array[pointer] = min -1;				
+            temp[temp.length - 1 - j] = max;                               
+        }
+        // 顯示結果
+        for(int i = 0;i < temp.length; i++)
+        {			
+            System.out.print(temp[i]+" ");
         }
     }    
 }
---
+++ HomeWork3/H3_111016041.java
@@ -0,0 +1,9 @@
+public class H3_111016041 {
+    public static void main(String [] args)
+    {
+        int A = 0;
+        int B = 0;
+
+        
+    }
+}
