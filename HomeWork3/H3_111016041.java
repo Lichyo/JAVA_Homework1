b901538c3afbabbeffe00bae197ebf5d85e7a336
Lichyo <chyoxuan@gmail.com>
2022-11-28 17:12:25 +0800

    Give Up fuck

--- HomeWork3/H3_111016041.java
+++ HomeWork3/H3_111016041.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+/*
+ * 班級：資二甲
+ * 姓名：李其祐
+ * 遊戲規則：輸入四個不重複的數字，然後每一輪將會公布在對的位置、錯的位置的數字共有多少個
+ *          一共可以猜十輪，若是十輪到了，或是輸入錯誤（輸入非整數、小於零的數、重複數字），將直接退出遊戲
+ * 自評分數：90（所有功能都有達到）
+ */
+
+
 public class H3_111016041 {
     public static void main(String [] args)
     {
         int A = 0;
         int B = 0;
+        int count = 0;
         String input;   // 使用者輸入
         int num [] = new int [4];     // 測試使用者輸入是否正確
         boolean check[]=new boolean[4];
@@ -16,11 +26,20 @@ public class H3_111016041 {
         // 由電腦隨機給出題目
 		for(int i=0;i<4;i++) question+=(int)(Math.random()*10);
 
+        System.out.println(question);
         // 使用者開始猜數字
         while(true)
         {
+            if(count == 10)
+            {
+                System.out.println("共計玩了十輪，遊戲即將終止");
+                System.out.print("正確答案為：" + question);
+                return;
+
+            }
             System.out.println("請輸入四個不重複整數，例如：1234");
             input = scn.next();
+            count++;
 
             // 確認輸入的都是整數
             for(int i = 0;i < 4; i++){			
@@ -44,15 +63,16 @@ public class H3_111016041 {
                 }
             }
 
-            // 確認輸入數字皆大於0
+            // 確認輸入數字皆大於等於0
             for(int i = 0; i < 4; i++)
             {
-                if(num[i] <= 0)
+                if(num[i] < 0)
                 {
                     inputCheck = true;
                 }
             }
 
+            // 若是輸入錯誤，將退出程式
             if(inputCheck == true)
             {
                 System.out.println("輸入錯誤，將退出程式");
@@ -91,16 +111,14 @@ public class H3_111016041 {
             if(A==4)
             {
                 System.out.println("恭喜答對");
+                System.out.println("一共玩了：" + count + " 輪");
                 break;
             }
             else
             {
                 System.out.println(A+"A"+B+"B");
+                System.out.println("第 "+ count +" 輪結束");
             }
         }
-
-        
-        
-        
     }
 }
--- HomeWork5/H5_111016041.java
+++ HomeWork5/H5_111016041.java
@@ -9,14 +9,26 @@ public class H5_111016041
          * infex -> prefix
          * 
          */
-
+        public static void push(char input, String stack [])
+        {
+            {
+            String [] copy_stack = new String [stack.length];
+            copy_stack = stack;
+            for(int i = 0; i < stack.length; i++)
+            {
+                while(i == stack.length-1)
+                {
+                    System.out.println("push done");
+                }
+                System.out.println("111");
+                stack[i+1] = copy_stack[i];
+            }
+            }
+        }
         Scanner scanner = new Scanner(System.in);
-        String testinput = "1 + 2 + 3";
         String [] input;
+        String stack [] = new String [200];
         
-        
-
-
         System.out.print("請輸入中序表示式，並以空格間隔每個運算子、元：");
         input = scanner.nextLine().split(" ");
         scanner.close();
@@ -25,8 +37,26 @@ public class H5_111016041
         {
             System.out.println(input[i]);
         }
+        Operator fun;
+        fun.push('+', stack);
+        
+
+
+
+
+
 
         // infix -> prefix alg
+        // while( loop_counter != 0)
+        // {
+        //     for(int i = 0; i < input.length; i++)
+        //     {
+        //         if(input[i] == "+" || input[i] == "-" || input[i] == "*" || input[i] == "/" || input[i] == "(" || input[i] == ")" )
+        //         {
+        //             operator_operand_flag = true;
+        //         }
+        //     }
+        // }
 
 
         
